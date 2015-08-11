aa6a885aca57e32213395c820841355a1e960d67
suqun <215144509@qq.com>
2015-08-11 22:25:12 +0800

    strategy

---
+++ src/test/com/larry/structural/strategy/APPTest.java
@@ -0,0 +1,16 @@
+package com.larry.structural.strategy;
+
+import org.junit.Test;
+
+/**
+ * Created by larry on 15-8-11.
+ *
+ */
+public class APPTest {
+
+    @Test
+    public void testMain() throws Exception {
+        String[] args = {};
+        APP.main(args);
+    }
+}
