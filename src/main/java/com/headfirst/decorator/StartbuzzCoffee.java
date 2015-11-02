ed8fd4f7d34b7378e6140593817c38f36df2df19
suqun <215144509@qq.com>
2015-11-02 23:01:24 +0800

    adding Head First decorator

--- src/main/java/com/headfirst/decorator/StartbuzzCoffee.java
+++ src/main/java/com/headfirst/decorator/StartbuzzCoffee.java
@@ -3,6 +3,16 @@ package com.headfirst.decorator;
 /**
  * Created by larry on 11/2/15.
  * 供应咖啡
+ *
+ * OO基础：
+ *        抽象，封装，多态，继承
+ * OO原则：
+ *       1，找出应用中可能需要变化之处，把它们独立出来，不要和那些不变化的代码混合在一起。  封装变化
+ *       2，针对接口编程，而不是针对实现编程
+ *       3，多用组合，少用继承
+ *       4，为交互对象之间的松耦合设计而努力 ——松耦合设计更有弹性，更能应对变化
+ *       5，对扩展开放，对修改关闭
+ * OO模式：装饰模式——动态的讲责任附加到对象上。想要扩展功能，装饰者提供有别于继承的另一种选择
  */
 public class StartbuzzCoffee {
     public static void main(String[] args) {
