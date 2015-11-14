87282126a023d125187abafc3152d8628d859be0
suqun <215144509@qq.com>
2015-11-14 23:26:08 +0800

    garage door open command

--- src/main/java/com/headfirst/command/simpleremote/GarageDoorOpenCommand.java
+++ src/main/java/com/headfirst/command/simpleremote/GarageDoorOpenCommand.java
@@ -5,6 +5,7 @@ package com.headfirst.command.simpleremote;
  * 车库开门的命令
  * 这个ConcreCommand定义了动作和接收者之间的绑定关系。
  * 调用者只要调用execute方法就可以发出请求，然后由ConcreCommand调用接收者的一个或多个动作。
+ *
  */
 public class GarageDoorOpenCommand implements Command {
     GarageDoor door;
